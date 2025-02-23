472fa22f4ff0b6b78f217281c5d905ecc7831455
Israa <israa.draz@richmond.edu>
2025-02-23 01:57:24 -0500

    Tree.gd and tree scene

--- scripts/TyTempMain.gd
+++ scripts/TyTempMain.gd
@@ -1,19 +1,24 @@
 extends Node
-@export var star_scene : PackedScene  # Assign the star scene in the inspector
-@export var time : Timer
+
+@export var star_scene: PackedScene   # Assign the star scene in the inspector
+
+# Get a reference to the Tree instance (must be a child of TyTempMain)
+onready var tree = $Tree  
+
 func _ready():
-	spawn_star()
-	spawn_star()
-	spawn_star()
-	spawn_star()
-	spawn_star()
-	spawn_star()
-	
-func _process(delta):
-	pass
+	# Spawn several stars on startup. Alternatively, use a Timer for periodic spawning.
+	for i in range(6):
+		spawn_star()
 
 func spawn_star():
-	var star = star_scene.instantiate() as Node2D  # Ensure it's the correct type
-	add_child(star)
+	var star_instance = star_scene.instantiate() as Node2D
+	add_child(star_instance)
 	
-	star.position = Vector2(randi_range(50, 1230), -50) # Assign a random position at the top of the screen
+	# Position star at a random x at the top of the screen.
+	star_instance.position = Vector2(int(rand_range(50, 1230)), -50)
+	
+	# Connect the star's collected signal to our callback.
+	star_instance.connect("collected", self, "_on_star_collected")
+	
+func _on_star_collected(points: int):
+	tree.on_star_collected(points)
